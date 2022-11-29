603560069d15377ca1098f5564da308703f458bd
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2022-11-28 23:13:17 -0600

    Header guards
    Also fixed line clear for IDO to stop being annoying

--- include/common.h
+++ include/common.h
@@ -1,2 +1,5 @@
+#ifndef COMMON_H
+#define COMMON_H
 #include <ultra64.h>
 #include "variables.h"
+#endif
--- include/variables.h
+++ include/variables.h
@@ -1,3 +1,6 @@
+#ifndef VARIABLES_H
+#define VARIABLES_H
 extern s32 D_800DCD04;
 extern s32 D_800DCD08;
 extern s32 D_800DCD0C;
+#endif
